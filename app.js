af9b8a165f27cdd0cf18a303d9579564207ebd16
Farooq Ahmed <03174748082ali@gmail.com>
2025-06-29 09:07:41 -0700

    completed work

--- app.js
+++ app.js
@@ -1,15 +1,74 @@
 // Object Array
 
-const student = {
-    fullName: "Ali",
-    age: 20,
-    totalMarks: 80,
-    isPass: true,
-};
+// const student = {
+//     fullName: "Ali",
+//     age: 20,
+//     totalMarks: 80,
+//     isPass: true,
+// };
 
-console.log(student);
+//console.log(student);
 
-//   Simple key value
-//console.log(student.fullName);
+//   Simple key value printed
+
+//console.log(student.fullName);   // Original name value (Ali)
 
 
+//student["fullName"] = "Ahmed";     // Change value (Ahmed) 
+//console.log(student);
+
+
+// Simple key value printed
+
+//console.log(student.age);      // Original age value (20)
+
+//student["age"] = student["age"] + 1;    // Increase age key value + 1 (21)
+//console.log(student["age"]);
+ 
+//student["age"] = student["age"] - 1;      // Decrease age key value - 1 (19)
+//console.log(student["age"]);
+
+
+// Simple key value print
+
+//console.log(student.totalMarks);        // Original age key value (80)
+
+//student["totalMarks"] = student["totalMarks"] + 10;   // Increase Marks key value + 10 now (90) 
+//console.log(student["totalMarks"]);
+ 
+//student["totalMarks"] = student["totalMarks"] -10;    // Decrease Marks key value - 10 now (70)
+//console.log(student["totalMarks"]);
+
+
+// Simple isPass key value print
+
+//console.log(student.isPass);           // Original isPass key value (true)
+
+
+//student["isPass"] = "false";         // Change isPass key value now(false)  
+//console.log(student["isPass"]);
+
+
+// const car = {
+//     fullName: "BMW",
+//     price: 1000000000,
+//     model: 2025,
+//     revealed: "Bayerische Motoren Werke",
+// };
+
+// const bike = {
+//     fullName: "Honda CD70",
+//     price: 180000,
+//     model: 2025,
+//     revealed : "Atlas Honda"
+
+// };
+
+// const cycle = {
+//     fullName: "SOHRAB Cycle",
+//     price: 20000,
+//     model: 2015,
+//     revealed: " Pakistan Cycle Industrial",
+// };
+
+// console.log(car, bike, cycle);
