6468e0a48579929ce040530d58dd68a8a12687e4
Farooq Ahmed <03174748082ali@gmail.com>
2025-06-29 08:12:16 -0700

    work started

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // Object Array
 
+const student = {
+    fullName: "Ali",
+    age: 20,
+    totalMarks: 80,
+    isPass: true,
+};
+
+console.log(student);
+
+//   Simple key value
+//console.log(student.fullName);
+
 
